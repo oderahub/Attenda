07c12884c0d0fb23603b32c0f447d4c465029ed3
oderahub <okere_chidera@yahoo.com>
2025-12-03 18:49:13 +0100

    refactor: update block explorer metadata to Scaffold-Celo
    Updated block explorer description from Scaffold-Lisk to Scaffold-Celo.

--- packages/nextjs/app/blockexplorer/layout.tsx
+++ packages/nextjs/app/blockexplorer/layout.tsx
@@ -2,7 +2,7 @@ import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
   title: "Block Explorer",
-  description: "Block Explorer created with 🏗 Scaffold-Lisk",
+  description: "Block Explorer created with 🏗 Scaffold-Celo",
 });
 
 const BlockExplorerLayout = ({ children }: { children: React.ReactNode }) => {
